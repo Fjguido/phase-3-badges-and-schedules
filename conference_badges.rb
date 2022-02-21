23b61fe8a1fc145f6b4786a51ccdb858085ad9e9
Francis Guido <fjguido97@gmail.com>
2022-02-21 17:05:12 -0500

    commit tree cbde2ac87441cc43fd3d892877daf98dfc779d34

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+def batch_badge_creator(array_names)
+    badge_names = []
+    array_names.each { |name| badge_names.push("Hello, my name is #{name}.")}
+    badge_names
+end
+def assign_rooms(array_speakers)
+    new_array = []
+  array_speakers.each_with_index { |speaker, index| new_array.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!") }
+  new_array
+end 
+def printer(array_of_names)
+    batch_badge_creator(array_of_names).each {|person| puts person}  
+    assign_rooms(array_of_names).each {|person| puts person}
+end
+ printer(['a','b'])
